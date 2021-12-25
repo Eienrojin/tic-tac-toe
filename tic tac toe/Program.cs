46db5231bab7f7857ce9b4d8969af5ccfb74e852
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-12-25 20:50:30 +0300

    Починил логику, добавил меню

--- tic tac toe/Program.cs
+++ tic tac toe/Program.cs
@@ -5,18 +5,61 @@
     { '#', '#', '#' }
 };
 bool isPlaysCross = false;
+bool isGameOver = false;
 
 int heigth = playField.GetLength(0);
 int weigth = playField.GetLength(1);
 
-for (; ; )
+//Вся логика начинается отсюда
+MainMenu();
+
+for (;;)
 {
     DefalthField();
     PrintField();
     InitNum();
     VictoryLogic();
-}
 
+    if (isGameOver == true)
+    {
+        Console.Clear();
+        MainMenu();
+        DefalthField();
+    }
+}
+//И заканчивается здесь
+
+void MainMenu()
+{
+    for (; ; )
+    {
+        isGameOver = false;
+        int menuChoice = 0;
+        string menuTitle = @"  _______         ______              ______         
+ /_  __(_)____   /_  __/___ ______   /_  __/___  ___ 
+  / / / / ___/    / / / __ `/ ___/    / / / __ \/ _ \
+ / / / / /__     / / / /_/ / /__     / / / /_/ /  __/
+/_/ /_/\___/    /_/  \__,_/\___/    /_/  \____/\___/ 
+                                                     ";
+
+        Console.Write(menuTitle);
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Добро пожаловать в крестики нолики!" + "\n1. Начать игру" + "\n2. Выйти из игры");
+        menuChoice = int.Parse(Console.ReadLine());
+
+        if (menuChoice == 1)
+        {
+            Console.Clear();
+            break;
+        }
+
+        if (menuChoice == 2)
+            Environment.Exit(0);
+    }
+
+
+}
 void DefalthField()
 {
     char[,] playField =
@@ -28,6 +71,7 @@ void DefalthField()
 }
 void PrintField()
 {
+    Console.Clear();
     Console.Write($" \t");
 
     for (int i = 0; i <= 2; i++)
@@ -57,8 +101,8 @@ void InitNum()
         Console.WriteLine("Сейчас ход ноликов");
     }
 
-    int y = 0; 
-    int x = 0;
+    int y = -1; 
+    int x = -1;
 
     for (; ; )
     {
@@ -94,27 +138,20 @@ void InitNum()
         isPlaysCross = true;
     }
 }
-void VictoryMessage(string player)
-{
-    if (player == "Zero")
-    {
-        Console.WriteLine("Нолики победили!");
-    }
-    if (player == "Cross")
-    {
-        Console.WriteLine("Крестики победили!");
-    }
-}
 void VictoryLogic()
 {
     int charZeroCounter = 0;
     int charCrossCounter = 0;
 
-    //Логика вычисления победы (здесь для строк)
     for (int i = 0; i < heigth; i++)
     {
+        //Логика вычисления победы для строк
         for (int j = 0; j < weigth; j++)
         {
+            //Сброс очков при переходе на следующую строку
+            charZeroCounter = 0;
+            charCrossCounter = 0;
+
             if (playField[i, j] == 'O')
             {
                 charZeroCounter++;
@@ -125,6 +162,28 @@ void VictoryLogic()
             }
 
             if (playField[i, j] == 'X')
+            {
+                charCrossCounter++;
+            }
+            else
+            {
+                charCrossCounter = 0;
+            }
+        }
+
+        //Логика вычисления победы для столбцов
+        for (int j = 0; j < weigth; j++)
+        {
+            if (playField[j, i] == 'O')
+            {
+                charZeroCounter++;
+            }
+            else
+            {
+                charZeroCounter = 0;
+            }
+
+            if (playField[j, i] == 'X')
             {
                 charCrossCounter++;
             }
@@ -143,35 +202,13 @@ void VictoryLogic()
             }
         }
 
-        //Логика вычисления победы для столбцов
-        for (int j = 0; j < weigth; j++)
+        if (charZeroCounter == 3)
         {
-            if (playField[i, j] == 'O')
-            {
-                charZeroCounter++;
-            }
-            else
-            {
-                charZeroCounter = 0;
-            }
-
-            if (playField[i, j] == 'X')
-            {
-                charCrossCounter++;
-            }
-            else
-            {
-                charCrossCounter = 0;
-            }
-
-            if (charZeroCounter == 3)
-            {
-                VictoryMessage("Zero");
-            }
-            if (charCrossCounter == 3)
-            {
-                VictoryMessage("Cross");
-            }
+            VictoryMessage("Zero");
+        }
+        if (charCrossCounter == 3)
+        {
+            VictoryMessage("Cross");
         }
     }
 
@@ -193,5 +230,20 @@ void VictoryLogic()
     if (playField[2, 0] == 'X' & playField[1, 1] == 'X' & playField[0, 2] == 'X')
     {
         VictoryMessage("Zero");
+    }
+}
+void VictoryMessage(string player)
+{
+    if (player == "Zero")
+    {
+        Console.WriteLine("Нолики победили!" + "\nНажмите любую клавишу, чтобы вернуться в меню...");
+        Console.ReadKey();
+        isGameOver = true;
+    }
+    if (player == "Cross")
+    {
+        Console.WriteLine("Крестики победили!" + "\nНажмите любую клавишу, чтобы вернуться в меню...");
+        Console.ReadKey();
+        isGameOver = true;
     }
 }
